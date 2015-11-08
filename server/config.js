9212f4cb8c394e1afc98399ba157d493d81f8f81
geniusburger <jdst8-github@yahoo.com>
2015-11-07 22:33:19 -0800

    Add other config default values

--- server/config.js
+++ server/config.js
@@ -5,5 +5,17 @@ exports.database = {
 		user: 'username',
 		password: 'password',
 		database: 'dbname'
+	},
+	mysql: {
+		dir: 'C:\\xampp\\mysql\\bin',
+		exe: 'mysql.exe',
+		ini: 'my.ini',
+		user: 'username',
+		pass: 'password'
 	}
 };
+
+exports.gcm = {
+	apiKey: 'apikey',
+	senderID: 'senderid'
+}
