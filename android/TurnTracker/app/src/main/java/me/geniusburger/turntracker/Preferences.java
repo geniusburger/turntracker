e987635e85f3430f6f326055382e256e22bd64ce
geniusburger <jdst8-git@yahoo.com>
2016-03-23 22:35:55 -0700

    Add a retry button to the settings

--- android/TurnTracker/app/src/main/java/me/geniusburger/turntracker/Preferences.java
+++ android/TurnTracker/app/src/main/java/me/geniusburger/turntracker/Preferences.java
@@ -10,6 +10,7 @@ public class Preferences {
 
     public static final String KEY_ANDROID_TOKEN = "android.token";
     public static final String KEY_ANDROID_TOKEN_SENT_TO_SERVER = "android.token.senttoserver";
+    public static final String KEY_ANDROID_TOKEN_RETRY = "android.token.retry";
     public static final String ANDROID_REGISTRATION_COMPLETE = "android.registration.complete";
 
     public static final String KEY_SERVER_IP = "server.ip";
