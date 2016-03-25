dff817828a10fff914cf587dd7a2e248cf6e0dd8
geniusburger <jdst8-git@yahoo.com>
2016-03-24 21:10:30 -0700

    fix option

--- server/cron.js
+++ server/cron.js
@@ -2,13 +2,10 @@ var Curl = require('node-libcurl').Curl;
 var curl = new Curl();
 
 curl.setOpt('URL', 'http://localhost:3000/api/remind');
-curl.setOpt('CURLOPT_PUT', 1);
+curl.setOpt('PUT', 1);
 
 curl.on('end', function( statusCode, body, headers ) {
 	console.info(statusCode);
-	console.info('---');
-	console.info(body.length);
-	console.info('---'); 
 	this.close();
 });
  
