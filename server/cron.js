16f645fe9437c66fd46fa769226cd5e91238a97d
geniusburger <jdst8-git@yahoo.com>
2016-03-24 21:00:13 -0700

    change order

--- server/cron.js
+++ server/cron.js
@@ -1,8 +1,8 @@
 var Curl = require('node-libcurl').Curl;
 var curl = new Curl();
 
-curl.setOpt('CURLOPT_PUT', 1);
 curl.setOpt('URL', 'http://localhost:3000/api/remind');
+curl.setOpt('CURLOPT_PUT', 1);
 
 curl.on('end', function( statusCode, body, headers ) {
 	console.info(statusCode);
